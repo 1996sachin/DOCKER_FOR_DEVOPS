7a5f64ad4391926752eea6461fdd553159690293
Sachin Singh <punksoul2580@gmail..com>
2025-11-09 21:57:44 +0545

    updated

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+
+import os
+
+app_version = os.environ.get('APP_VERSION', 'Unknown')
+environment = os.environ.get('ENVIRONMENT', 'Unknown')
+
+print(f"Running App version {app_version} in {environment}environment.")
+
+
+
